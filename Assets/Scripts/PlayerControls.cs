606447cbed4fbaee842f8457cce28bd66bbfd697
oliver bullows <oliverbullows@hotmail.com>
2024-03-09 16:52:44 +0000

    First commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    Vector2 Movement;
+    public Rigidbody2D RB;
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement.x = Input.GetAxis("Horizontal");
+        Movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        RB.MovePosition(RB.position + Movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Right.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Right : MonoBehaviour
+{
+    public GameObject Ship;
+    public GameObject ShipRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
